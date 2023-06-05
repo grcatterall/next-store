4fbf87473e44eb73144c8f116527170b427dc58c
Graham Catterall <graham.catterall@sky.uk>
2023-06-05 10:27:33 +0100

    Category page work

--- src/app/category/[category]/page.tsx
+++ src/app/category/[category]/page.tsx
@@ -25,20 +25,16 @@ const Category = () => {
     return (
         <div className="mx-auto w-4/5">
             <h1 className="text-xl font-bold">{categoryName}</h1>
-            <div className='mt-3 flex w-max flex-wrap'>
+            <div className='mt-3 gird-cols-1 xl:grid-cols-4 grid xl:gap-2 mb-6'>
                 {products.map((product: Product) => {
                     return (
-                        <div className="w-full flex-1 max-w-sm mt-6 lg:mt-8 bg-gray-100 rounded shadow-lg" key={product.id}>
+                        <div className="w-full max-w-sm mt-6 lg:mt-8 bg-gray-100 rounded shadow-lg" key={product.id}>
                             <div className="inline-block bg-white rounded-lg w-full p-4">
                                 <img src={product.image} alt={product.title} className="w-full h-80 object-contain" />
                             </div>
-                            <div className="p-12 lg:p-8 lg:mx-4 md:mx-2 xl:p-12">
-                                <div className="mt-4 font-extrabold text-2xl tracking-wide">
+                            <div className="p-2 lg:p-3 lg:mx-4 md:mx-2 xl:p-4">
+                                <div className="mt-4 font-bold text-2xl tracking-wide">
                                     {product.title}
-                                </div>
-                                <div className="text-sm">
-                                    We guarantee that every designer you ever work with will be an awesome member for your team. We
-                                    conduct personal interview with every designer to ensure that we only get the best.
                                 </div>
                             </div>
                         </div>
