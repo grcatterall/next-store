33fda5a9dc47acfb20448bd04fb3baddc74dc179
Graham Catterall <grahamcatterall@Grahams-MacBook-Air-2.local>
2023-11-03 20:16:20 +0000

    Removed dangerous line

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ const Home = () => {
   return (
     <>
       <main className="min-h-screen">
-        <Carousel />
 
         <div className="flex h-full flex-col items-center justify-between p-24 pt-2">
         <div className="p-5 sm:px-10 md:px-20" id="companies">
